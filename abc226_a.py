98d4392c3cf695a50f12d8d51d064c4c8b5effa8
torippi <19960215yusuke@gmail.com>
2021-11-14 21:49:44 +0900

    メソッド化し、プログラムをシンプルに変更。さらに、メソッドの説明を追記￥

--- abc226_a.py
+++ abc226_a.py
@@ -1,8 +1,13 @@
 import math
 
-#int型で受け取るとき
-s = float(input()) 
-n = 1  # 切り捨てしたい桁
-y = math.floor(s * 10 ** n) / (10 ** n)
-y += 0.000005
-print(f'{round(y)}')
+def roundFirstDenical(inputNum):
+    '''小数点第一位で四捨五入
+
+    :param: inputNum: 四捨五入したい値
+    :return: 小数点第一位で四捨五入した値（int）
+    '''
+    y = round(math.floor(inputNum * 10) / (10) + 0.000005)
+    return y
+
+inputNum = float(input())
+print(roundFirstDenical(inputNum))
